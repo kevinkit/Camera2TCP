2bacd00332fad1fe74dfd9c864e12c323c0ee64a
Kevin <kevinahoefle@gmail.com>
2017-04-17 16:32:06 +0200

    Letting numpy do the work
    Numpy does now the conversion from byte to image and therefore it is very very fast!

--- clientA.py
+++ clientA.py
@@ -24,29 +24,9 @@ while True:
     except Exception:
         print("host unreachable...will try...forever...")
         continue;
+    
+    img = np.frombuffer(data,dtype=np.uint8).reshape(240,424,3)
 
-    #Convert from ascii to string
-    asstr = binascii.hexlify(data)
-    #Split up after each byte couple
-    n = 2
-    split = [asstr[i:i+n] for i in range(0, len(asstr), n)]
-    #Convert each byte couple to integer from its hex representation    
-    asint = [];
-    for i in split:    
-        asint.append(int(i,16))   
-        
-    #Reshape into red,green and blue
-    try:
-        red = np.asarray(asint[::3]).reshape(240,424);
-        green = np.asarray(asint[1::3]).reshape(240,424);
-        blue = np.asarray(asint[2::3]).reshape(240,424);
-    except ValueError:
-        continue;
-        
-    #Reshape into an Image representation for opencv
-    img = np.transpose(np.asarray([red,green,blue],dtype=np.uint8),axes=(1, 2, 0))
-
-    #Show image
     cv2.imshow('something',img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
